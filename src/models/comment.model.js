a11109a153cafd39fe2ed81115013384e4fef98f
Hafiz Tanzeel Shamshad <hafiztanzeel@gmail.com>
2025-09-05 19:23:49 +0500

    feat: Add comment and tweet models with mongoose schema

---
+++ src/models/comment.model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema({
+    
+    content: {
+        type: String,
+        required: true,
+    },
+    Video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+
+}, {timestamps: true});
+
+commentSchema.plugin(mongooseAggregatePaginate);
+export const CommentModel = mongoose.model("Comment", commentSchema);
