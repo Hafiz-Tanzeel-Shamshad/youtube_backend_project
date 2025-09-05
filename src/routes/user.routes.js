8bf6449f36c7bb01c0e75f05c82e674c71811d80
Hafiz Tanzeel Shamshad <hafiztanzeel@gmail.com>
2025-09-05 17:18:13 +0500

    feat: Add secure routes for changing password, retrieving current user, and updating account details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatarImage } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatarImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import  {verifyJWT}  from "../middlewares/auth.middleware.js";
 
@@ -18,5 +18,14 @@ router.route("/login").post(loginUser);
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").put(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatarImage);
+router.route("/update-cover").patch(verifyJWT, upload.single("coverImage"), updateUserAvatarImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
