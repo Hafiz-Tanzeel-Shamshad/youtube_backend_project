e91503eff81e0400b615ebda86c91f25b01a1a72
Hafiz Tanzeel Shamshad <hafiztanzeel@gmail.com>
2025-09-05 17:44:50 +0500

    fix: Update logoutUser to remove refresh token from database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -155,7 +155,7 @@ const logoutUser = asyncHandler( async (req, res) => {
    await User.findByIdAndUpdate(
     req.user._id,
         { 
-            $set: { refreshToken: undefined }
+            $unset: { refreshToken: 1 } // remove refresh token from DB
         },
         {new: true}
     );
